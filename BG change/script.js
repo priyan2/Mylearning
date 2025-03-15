8bef86e222ed661b639a0eaa8b1eb720f3d0a2a6
Shanmugapriyan <agsshanmu@gmail.com>
2025-03-15 13:47:43 +0530

    Bg color changer
    When we click button background color of body changes, used DOM to make change

---
+++ BG change/script.js
@@ -0,0 +1,10 @@
+function greenColor(){
+    document.body.style.backgroundColor = "green";
+     
+}
+function blueColor(){
+    document.body.style.backgroundColor = "skyblue"
+}
+function greyColor(){
+    document.body.style.backgroundColor = "grey";
+}
